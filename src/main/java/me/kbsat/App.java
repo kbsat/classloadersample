460b68b3559358d40376994b8940fe4f2fa86394
BeomsooKim <kbsat@naver.com>
2022-01-02 17:00:05 +0900

    클래스 로더

---
+++ src/main/java/me/kbsat/App.java
@@ -0,0 +1,17 @@
+package me.kbsat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
+
+    }
+}
